6a255b6c93e33384a0bbe1c3f3942724654e366f
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-12-31 00:43:52 +0100

    Flow of Elements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Flux<Integer> justFlux = Flux.just(1, 2, 3, 4); //static stream of four elements
+        Mono<Integer> justMono = Mono.just(1);
+
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(elements::add);
     }
 }
