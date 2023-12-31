6b00b5ac18bf1d46a75f29c0f97fcbecc0857ed7
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-12-31 13:03:41 +0100

    Creating a ConnectableFlux

---
+++ src/main/java/HotStreams.java
@@ -0,0 +1,18 @@
+package main.java;
+
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+public class HotStreams {
+    public static void main(String[] args) {
+        //Creating a ConnectableFlux
+        ConnectableFlux<Object> publish = Flux.create(fluxSink -> {
+                    while(true) {
+                        fluxSink.next(System.currentTimeMillis());
+                    }
+                })
+                .publish();
+        publish.subscribe(System.out::println);
+        publish.connect();
+    }
+}
