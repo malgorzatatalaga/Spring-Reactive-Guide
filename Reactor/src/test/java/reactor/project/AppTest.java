cb86af5eb3d56b99d202d90260d725ebd35fcbb0
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-05 00:54:50 +0100

    Reactor Module

---
+++ Reactor/src/test/java/reactor/project/AppTest.java
@@ -0,0 +1,21 @@
+package reactor.project;
+
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+    * Rigourous Test :-)
+    */
+    @DisplayName("Dummy test successful")
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
