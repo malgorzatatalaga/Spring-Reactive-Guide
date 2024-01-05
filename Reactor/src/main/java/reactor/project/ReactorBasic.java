259e15526fc9c3f03c061d571223f63e35170f70
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2024-01-05 01:01:03 +0100

    Reactor in action - "Spring 5.0 Projects"

---
+++ Reactor/src/main/java/reactor/project/ReactorBasic.java
@@ -0,0 +1,22 @@
+package reactor.project;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReactorBasic {
+    private static List<String> carModels = Arrays.asList(
+            "Era","Magna","Sportz","Astha","Astha(O)");
+    public static void main(String args[]) {
+        Flux<String> fewWords = Flux.just("Hello", "World");
+        Flux<String> manyWords = Flux.fromIterable(carModels);
+        Mono<String> singleWord = Mono.just("Single value");
+        fewWords.subscribe(t->System.out.println(t));
+        System.out.println("-----------------------------");
+        manyWords.subscribe(System.out::println);
+        System.out.println("-----------------------------");
+        singleWord.subscribe(System.out::println);
+    }
+}
